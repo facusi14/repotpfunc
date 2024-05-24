03901144b01d60b03aed390fbb3e6c7e4c902680
Facundo <facundogomez1455@gmail.com>
2024-05-23 21:51:04 -0300

    commit de funciones

--- funcionesPrincipal.py
+++ funcionesPrincipal.py
@@ -1,3 +1,8 @@
 import funcionesBiblioteca
-
-pass
+def menu_opciones(a,b):
+    print(f"""1. Ingresar 1er operando a={a}
+          2. Ingresar 2do operando b={b}
+          3. Calcular todas las operaciones 
+          4. Informar resultados 
+          5. Salir""")
+menu_opciones()
